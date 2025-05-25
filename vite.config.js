8bd569f91396defc5ceaad3d8a8d73bb5aac12cd
Juan Pablo Mosquera Hinestroza <Juanpablomosquera@estudiante.uniajc.edu.co>
2025-05-25 20:41:21 +0000

    pagina probably done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/web.github.io/', // base para GitHub Pages
   server: {
     proxy: {
       '/api': {
